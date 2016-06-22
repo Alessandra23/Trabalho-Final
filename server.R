7049dc1cebb7b7d2ec21b42c614173a52d8656e4
Alessandra23 <alessalemos23@gmail.com>
2016-06-22 05:50:05 -0300

    Novas alterações
    Ainda falta consertar os gráficos e acrescentar a tabela.

--- server.R
+++ server.R
@@ -1,73 +1,159 @@
-shinyServer(
-  function(input, output, session){
-    
-    RDist<- reactive({
-      dist <- switch(input$dist,
-                     norm = rnorm,
-                     binom = rbinom,
-                     gama = rgamma,
-                     rnorm)
-      
-      dist(input$n, ...)
-    })
-    
-    output$plot1 <- renderPlot({
-      
-      Tdist <- input$dist
-      Tamos <- input$tsample
-      Namos <- input$namostra
-      MedA <- numeric(Tamos)
-      Eixox <- c(1:Tamos)
-      
-      if(Tdist == "Normal"){
-        for(i in 1:Namos){
-          vetA <- rnorm(Tamos, mean = as.numeric(input$med), 
-                        sd = as.numeric(input$dp))
-          MedA[i] <- mean(vetA)
-        }
-        
-      }else{
-        if(Tdist == "Exponencial"){
-          for(i in 1:Namos){
-            vetA <- rexp(Tamos, rate = 1/as.numeric(input$lambda))
-            MedA[i] <- mean(vetA)
-          }
-          
-        }else{
-          for(i in 1:Namos){
-            vetA <- rbinom(Tamos, as.numeric(input$n), as.numeric(input$p))
-            MedA[i] <- mean(vetA)
-          }
-          
-        }
-      }
-      
-      plot(Eixox,MedA)
-      #hist(MedA, col = "blue", main = "Histograma")
-    })
-    
-#----------------------------------------------------------------------------------------------------------#
-# Plotar o segundo gr�fico
-    
-    output$plot2 <- renderPlot({
-      
-      
-    })    
-    
-#----------------------------------------------------------------------------------------------------------#
-# Plotar o terceiro gr�fico    
-    
-    output$plot3 <- renderPlot({
-      
-      
-    })  
+library(plotly)
+require(ggplot2)
+library(shinythemes)
 
-#----------------------------------------------------------------------------------------------------------#
-# Gerar a tabela
+
+shinyServer(function(input, output){
+  
+  output$tex1 <- renderText({ 
+    "O Teorema Central do Limite diz que..."
+  })
+  
+  
+  output$med = renderUI({
+    if (input$dist == "rnorm"){sliderInput("med", "Média", min = -50, max = 50, value = 0)}
+  })
+  output$dp = renderUI({
+    if(input$dist == "rnorm"){sliderInput("dp", "Desvio padrão", min = 1, max = 40, value = 1)}
+  })
+  output$min = renderUI({
+    if (input$dist == "runif"){sliderInput("min","Minimo", min = -20, max = 20,value = 0)}
+  })
+  output$max = renderUI({
+    if (input$dist == "runif"){sliderInput("max", "Maximo", value = 2, min = 0, max = 20)}
+  })
+  
+ 
+  f1 <- function(dist, n, med, dp, min, max){
     
-    output$table <- renderTable({
+    val <- NULL
+    
+    if(dist == "rnorm"){
+      med <- input$med 
+      dp <- input$dp
+      n <- input$n
+      val <- rnorm(n, med, dp)
+    } else if(dist == "runif"){
+      min <- input$min
+      max <- input$max
+      n <- input$n
+      val <- runif(n, min, max) 
+    }
+    return(val)
+  } # Fim da função 1
+  
+  
+  rep_f1 <- repeatable(f1)
+  
+  pri <- reactive({
+    n <- input$n
+    return(rep_f1(input$dist, n, input$med, input$dp, input$min, input$max))
+  }) 
+  
+  
+  
+  
+  amostra <- reactive({
+    Dis <- pri()
+    N <- input$N
+    n <- input$n
+    return(replicate(N, sample(Dis, n, replace = T)))
+  }) 
+  
+  
+  # Gráfico 1
+  
+  output$plot1 <- renderPlotly({
+    nome_dist <- switch (input$dist,
+                         rnorm = "Distribuição Normal",
+                         runif = "Distribuição Uniforme"
+    )
+    
+    Dis <- pri()
+    mDist <- mean(Dis)
+    dpDist <- sd(Dis)
+    
+    Mi <- NULL
+    Ma <- NULL
+    erro <- F
+    
+    if(input$dist == "runif"){
+      Mi <- input$min
+      Ma <- input$max
+      if(Mi > Ma){
+        erro <- T
+      }
+    }
+    
+    if(erro){
       
-    })
+      plot(0,0,type = 'n', xlab = " ", ylab = " ")
+      text(0,0,"ERRO : O limite inferior é maior que o superior.", col = "red")
+      
+    } else {
+      
+      Df <- data.frame(x = Dis)
+      library(plotly)
+      g <- ggplot(Df, aes(x = x)) +
+        geom_histogram(aes(y = ..density..), binwidth = density(Df$x)$bw)+
+        geom_density(fill = "red", alpha = 0.2) +
+        xlab(" ") + ylab(" ") + 
+        ggtitle(" ")
+      p <-  ggplotly(g) ; p
+      
+    }
+    
+  }) # Fim do gráfico 1  
+  
+  
+  # Gráfico 2
+  
+  output$plot2 <- renderPlotly({
+    
+    Mi <- NULL
+    Ma <- NULL
+    erro <- F
+    
+    if(input$dist == "Uniforme"){
+      Mi <- input$min
+      Ma <- input$max
+      if(Mi > Ma){
+        erro <- T
+      }
+    }
+    
+    if(erro) 
+      return
+    else{
+      
+      nome_dist <- switch(input$dist,
+                          rnorm = "Distribuição Normal",
+                          runif = "Distribuição Uniforme")
+      N <- input$N
+      n <- input$n
+     
+      Dis <- pri()
+      mDist <- mean(Dis)
+      dpDist <- sd(Dis)
+      medis <- colMeans(amostra())
+      
+      med_amost <- mean(medis)
+      dp_amost <- sd(medis)
+      
+      Df2 <- data.frame(x = medis)
+      library(plotly)
+      ggplot(Df2, aes(x = x)) +
+        geom_histogram(aes(y = ..density..), binwidth = density(Df2$x)$bw)+
+        geom_density(fill = "red", alpha = 0.2) +
+        xlab(" ") + ylab(" ") +
+        ggtitle(" ")
+      ggplotly()
+      
+    }
     
     
-})
+  }) # Fim do gráfico 2
+  
+  
+  
+}) # Fim da seção
--- ui.R
+++ ui.R
@@ -1,35 +1,38 @@
 shinyUI(
-  pageWithSidebar(
-    headerPanel("Teorema do Limite Central"),
-    
+  
+  fluidPage( theme = shinytheme("cerulean"),
+  
+    headerPanel("Teorema Central do Limite"),
+  
     sidebarPanel(
-      selectInput("dist", "Selecione a distribuicao",
-                  choices = c("Normal", "Gama", "Uniforme")), 
-      sliderInput("tsample","Tamanho da amostra", 
-                  min = 1, max = 1000, value = 10, step = 10), 
-      sliderInput("namostra", "Numero de amostras",
-                  min = 1, max = 1000, value = 10, step = 10), 
+      radioButtons("dist", "Selecione a distribuição:",
+                   list("Normal" = "rnorm",
+                        "Uniforme" = "runif")),
       
-      uiOutput("med"), 
-      uiOutput("dp"),
-      uiOutput("min"),
-      uiOutput("max"),
+      uiOutput("min"), uiOutput("max"), uiOutput("med"), uiOutput("dp"),
+      sliderInput("n", "Tamanho da amostra", min = 1, max = 1000, value = 10),
       
-      conditionalPanel(condition = "input.dist == 'Normal'", 
-                       textInput("med", "Media da distribuicao", 5),
-                       textInput("dp", "Desvio padrao da distribuicao", 2)),
-      conditionalPanel(condition = "input.dist == 'Gama'",
-                       textInput("lambda","Parametro de escala", 1)),
-      conditionalPanel(condition = "input.dist == 'Uniforme'",
-                       textInput("n", "Numero de tentativas",5),
-                       textInput("p", "Parametro p da distribuicao",0.5))
+      fluidRow(shiny::column(4, offset = 4,
+                      sliderInput("N", "N",
+                                  min = 0, max = 50,
+                                  value = 10, animate = TRUE)))
+      
+     
     ),
+  
     
     mainPanel(
       tabsetPanel(type = "tabs", 
-                  tabPanel("Densidade", plotOutput("plot1")), 
-                  tabPanel("Distribuicao das amostras", plotOutput("plot2")), 
-                  tabPanel("Distribuicao amostral das medias", plotOutput("plot3")), 
-                  tabPanel("Tabela", tableOutput("table"))
+                  tabPanel("Gráficos", div(textOutput("tex1"), align = "center"),
+                           plotlyOutput("plot1"),
+                           plotlyOutput("plot2")), 
+                  tabPanel("Tabela", tableOutput("tab")))
       )
-)))
+    
+    
+    
+ ))
+
+
+# https://github.com/rstudio/shiny-examples/blob/master/052-navbar-example/ui.R
+# http://shiny.rstudio.com/articles/layout-guide.html
